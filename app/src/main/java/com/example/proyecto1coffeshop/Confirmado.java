3968297fc2d38bac355aa5bc48a2cf7ec45de556
AlbertoD10 <kevind2698@gmail.com>
2020-10-10 01:23:35 -0500

    Agregado RatingBar0

--- app/src/main/java/com/example/proyecto1coffeshop/Confirmado.java
+++ app/src/main/java/com/example/proyecto1coffeshop/Confirmado.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,14 +19,16 @@ import java.util.Random;
 
 public class Confirmado extends AppCompatActivity {
     TextView tvcodigo, tvhora;
-    Button btnVolverMenu;
+    Button btnVolverAlmenu;
+    RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmado);
         tvcodigo=(TextView) findViewById(R.id.tvcodigo);
         tvhora=(TextView) findViewById(R.id.tvhora);
-        btnVolverMenu=(Button) findViewById(R.id.btnVolverMenu);
+        btnVolverAlmenu=(Button) findViewById(R.id.btnVolverAlmenu);
+        ratingBar = findViewById(R.id.ratingBar);
 
         Random rand = new Random();
         int upperbound = 999999;
@@ -37,14 +40,22 @@ public class Confirmado extends AppCompatActivity {
         //String dateTime = SimpleDateFormat.format(calendar.getTime());
         Date dateTime = Calendar.getInstance().getTime();
         String formattedtime = DateFormat.getDateTimeInstance().format(dateTime);
-        tvhora.setText(formattedtime);
+        tvhora.setText("Hora de pedido: "+ formattedtime);
 
-        btnVolverMenu.setOnClickListener(new View.OnClickListener() {
+        btnVolverAlmenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity3();
             }
         });
+
+        ratingBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Confirmado.this, "Gracias por calificar la APP ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
     public void openActivity3(){
         Intent intent = new Intent(this, Activity3.class);
